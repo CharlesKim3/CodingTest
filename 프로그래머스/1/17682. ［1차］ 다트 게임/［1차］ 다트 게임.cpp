80e0a02838ac225f744ee7cb1d0c06bacdc673bf
CharlesKim3 <tpwls971105@naver.com>
2025-04-10 14:57:32 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.cpp
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.cpp
@@ -1,43 +1,41 @@
 #include <string>
-#include <cmath>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int solution(string dartResult) {
     int answer = 0;
-    
-    string tmp = "";
     vector<int> score;
+    string tmp = "";
     for(int i=0; i<dartResult.size(); ++i)
     {
-        if(48 <= dartResult[i] && dartResult[i] <= 57)
+        if('0' <= dartResult[i] && dartResult[i] <= '9')
             tmp += dartResult[i];
         
         if(dartResult[i] == 'S')
         {
             score.push_back(stoi(tmp));
             tmp = "";
-            
         }
         
         else if(dartResult[i] == 'D')
         {
-            score.push_back(pow(stoi(tmp), 2));
+            score.push_back(pow(stoi(tmp),2));
             tmp = "";
         }
         
         else if(dartResult[i] == 'T')
         {
-            score.push_back(pow(stoi(tmp), 3));
+            score.push_back(pow(stoi(tmp),3));
             tmp = "";
         }
         
         if(dartResult[i] == '*')
         {
-            int last = score.size() - 1;
-            for (int j = last; j >= last - 1; --j)
+            int idx = score.size();
+            for(int j= idx - 1; j >= idx - 2; --j)
             {
-                if (j < 0)
+                if(j < 0)
                     break;
                 score[j] *= 2;
             }
@@ -45,11 +43,11 @@ int solution(string dartResult) {
         
         else if(dartResult[i] == '#')
             score[score.size() -1] *= -1;
+            
     }
     
     for(auto& s : score)
         answer += s;
-    
     
     return answer;
 }
