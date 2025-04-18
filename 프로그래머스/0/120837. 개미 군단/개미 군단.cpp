5dd4290673991750f1638eef7d7254d69914550b
CharlesKim3 <tpwls971105@naver.com>
2025-04-18 14:33:50 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -5,27 +5,12 @@ using namespace std;
 
 int solution(int hp) {
     int answer = 0;
-    vector<int> ant = {5,3,1};
     
-    while(hp > 0)
-    {
-        if(hp >= 5)
-        {
-            answer += hp / ant[0];
-            hp %= ant[0];
-        }
-        
-        else if(hp >= 3)
-        {
-            answer += hp / ant[1];
-            hp %= ant[1];
-        }
-        
-        else
-        {
-            answer += hp / ant[2];
-            hp %= ant[2]; 
-        }
-    }
+    answer += hp / 5;
+    hp = hp % 5;
+    answer += hp / 3;
+    hp = hp % 3;
+    answer += hp;
+    
     return answer;
 }
