2a8a20f9ca26445ea1ca6e3d68a4ed9656f290de
CharlesKim3 <tpwls971105@naver.com>
2025-04-28 17:11:33 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.04 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
@@ -6,46 +6,41 @@ using namespace std;
 string solution(string new_id) {
     string answer = "";
     
-    // 1
     for(int i=0; i<new_id.size(); ++i)
     {
-        if ('A' <= new_id[i] && new_id[i] <= 'Z')
+        if('A' <= new_id[i] && new_id[i] <= 'Z')
             new_id[i] = new_id[i] + 32;
     }
     
-    // 2
-    for (int i = 0; i < new_id.size(); ++i) 
+    for(int i=0; i<new_id.size(); ++i)
     {
-        if (('a' <= new_id[i] && new_id[i] <= 'z') ||
-            ('0' <= new_id[i] && new_id[i] <= '9') ||
-            new_id[i] == '-' || new_id[i] == '_' || new_id[i] == '.')
+        if(('a' <= new_id[i] && new_id[i] <= 'z') || 
+           (48 <= new_id[i] && new_id[i] <= 57) || 
+           new_id[i] == '-' || new_id[i] == '_' || new_id[i] == '.')
             answer += new_id[i];
     }
     
-    // 3
-    string temp = "";
-    for (int i = 0; i < answer.size(); ++i) {
-        if (answer[i] == '.' && !temp.empty() && temp.back() == '.')
+    string tmp = "";
+    for(int i=0; i<answer.size(); ++i)
+    {
+        if(answer[i] == '.' && tmp.back() == '.')
             continue;
-        temp += answer[i];
+            
+        tmp += answer[i];
     }
-    answer = temp;
+    answer = tmp;
     
-    // 4
-    if (!answer.empty() && answer.front() == '.')
-        answer.erase(answer.begin());
-    if (!answer.empty() && answer.back() == '.')
+    if(answer.front() == '.')
+        answer = answer.substr(1);
+    if(answer.back() == '.')
         answer.pop_back();
     
-    //5
-    if (answer.empty())
+    if(answer.empty())
         answer = "a";
     
-    //6
     if(answer.size() >= 16)
-        answer = answer[14] == '.' ? answer.substr(0,14) : answer.substr(0,15); 
+        answer = answer[14] == '.' ? answer.substr(0,14) : answer.substr(0,15);
     
-    // 7 
     while(answer.size() < 3)
         answer += answer.back();
     
