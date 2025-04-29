33e6bf7b49e94527fed68770f244dfc0a561b9c4
CharlesKim3 <tpwls971105@naver.com>
2025-04-29 14:55:27 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -3,9 +3,10 @@ using namespace std;
 
 int main()
 {
-	long long count;
-	cin >> count;
-	cout << (count - 1) * count / 2 << endl << 2;
+	long long n;
+	cin >> n;
+	cout << (n * (n - 1)) / 2 << endl;
+	cout << 2;
 
 	return 0;
 }
