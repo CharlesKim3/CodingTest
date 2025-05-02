f9c32d29a225b81203562e8b6fc841f0e24d58f7
CharlesKim3 <tpwls971105@naver.com>
2025-05-02 10:22:19 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.09 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.05 ms
+메모리: 4.13 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 01일 16:53:44
+2025년 05월 02일 10:22:14
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -5,50 +5,51 @@ using namespace std;
 
 vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
     vector<int> answer;
-    unordered_map<string,int> m;
-    for(int i=0; i<terms.size(); ++i)
-        m[terms[i].substr(0,1)] = stoi(terms[i].substr(2));
+    unordered_map<string, int> m;
+    for(auto& t : terms)
+        m[t.substr(0,1)] = stoi(t.substr(2));
     
     for(int i=0; i<privacies.size(); ++i)
     {
         int year = stoi(privacies[i].substr(0,4));
         int month = stoi(privacies[i].substr(5,2));
         int date = stoi(privacies[i].substr(8,2));
-        int calculate = m[privacies[i].substr(11)];
-
-        if(calculate + month >= 13)
+        int cal = m[privacies[i].substr(11)];
+        
+        if(cal + month >= 13)
         {
-            if((calculate + month) % 12 != 0)
+            if((cal + month) % 12 == 0)
             {
-                year += (calculate + month) / 12;
-                month = (calculate + month) % 12;
+                year += cal / 12;
+                month += cal % 12;
             }
             else
             {
-                year += calculate / 12;
-                month += calculate % 12;
+                year += (cal + month) / 12;
+                month = (cal + month) % 12;
             }
         }
         else
-            month += calculate;
+            month += cal;
         
-        if(date - 1 == 0)
+        if(date == 1)
         {
-            month -= 1;
+            --month;
             date = 28;
         }
         else
             --date;
+        
         if(stoi(today.substr(0,4)) > year)
-            answer.push_back(i + 1);
+            answer.push_back(i+1);
         else if(stoi(today.substr(0,4)) == year)
         {
             if(stoi(today.substr(5,2)) > month)
-                answer.push_back(i + 1);
+                answer.push_back(i+1);
             else if(stoi(today.substr(5,2)) == month)
             {
                 if(stoi(today.substr(8,2)) > date)
-                    answer.push_back(i + 1);
+                     answer.push_back(i+1);
             }
         }
     }
