984e4bde176be5842dec88cedd8c9cd786d8eb6e
CharlesKim3 <tpwls971105@naver.com>
2025-05-09 12:34:09 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int X;
+	cin >> X;
+
+	int line = 1;
+	while (X - line > 0)
+	{
+		X -= line;
+		line++;
+	}
+	
+	if ((line & 1) == 1) cout << line + 1 - X << '/' << X;
+	else cout << X << '/' << line + 1 - X;
+	
+	return 0;
+}
