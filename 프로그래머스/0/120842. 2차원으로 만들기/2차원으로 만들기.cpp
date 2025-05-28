d05fe8ca00287df1f641937fedb2189cff227254
CharlesKim3 <tpwls971105@naver.com>
2025-05-28 17:25:41 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<int> num_list, int n) {
+    vector<vector<int>> answer;
+    
+    int idx = 0;
+    for(int i=0; i<num_list.size() / n; ++i)
+    {
+        vector<int> tmp;
+        for(int j=0; j<n; ++j)
+        {
+            tmp.push_back(num_list[idx]);
+            ++idx;
+        }
+        answer.push_back(tmp);
+    }
+    
+    return answer;
+}
