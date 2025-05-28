526ee8e63e8cbab118c34cdbfba617c1ca816c5d
CharlesKim3 <tpwls971105@naver.com>
2025-05-28 17:36:31 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int hour, min;
+	cin >> hour >> min;
+
+	if (min - 45 >= 0)
+		cout << hour << " " << min - 45;
+	else
+	{
+		if(hour != 0)
+			cout << hour - 1 << " " << 60 + (min - 45);
+		else
+			cout << 24 - 1 << " " << 60 + (min - 45);
+	}
+		
+
+	return 0;
+}
