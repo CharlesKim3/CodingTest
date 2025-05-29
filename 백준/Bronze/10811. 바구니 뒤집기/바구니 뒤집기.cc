b910de5ac74d88ac9594f4ed4ea36d3d06ff5bd5
CharlesKim3 <tpwls971105@naver.com>
2025-05-29 15:43:05 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -6,33 +8,18 @@ int main()
 	int n, m;
 	cin >> n >> m;
 	
-	int* basket = new int[n];
-	for (int i = 0; i <n; ++i)
-		basket[i] = i + 1;
-
-	for (int i = 0; i < m; ++i)
-	{
-		int begin, end;
-		cin >> begin >> end;
-		int* tmp = new int[end - begin + 1];
-		int idx = 0, idx2 = 0;
-		for (int j = end; j >= begin; --j)
-		{
-			tmp[idx] = basket[j - 1];
-			++idx;
-		}
-		
-		for (int k = begin -1; k < end; ++k)
-		{
-			basket[k] = tmp[idx2];
-			++idx2;
-		}	
-	}
-	
+	vector<int> basket;
 	for (int i = 0; i < n; ++i)
-	{
-		cout << basket[i] << " ";
-	}
+		basket.push_back(i + 1);
 	
+	for (int k = 0; k < m; ++k)
+	{
+		int i, j;
+		cin >> i >> j;
+		reverse(basket.begin() + i - 1, basket.begin() + j);
+	}
+
+	for (auto& b : basket)
+		cout << b << " ";
 	return 0;
 }
