c51c6d8c6b140bfc9abc68679a553213817fbe2f
CharlesKim3 <tpwls971105@naver.com>
2025-05-30 17:47:15 +0900

    [level 2] Title: 올바른 괄호, Time: 0.53 ms, Memory: 3.98 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,23 @@
+#include<string>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> stk;
+    for(auto c : s)
+    {
+        if(c == '(')
+            stk.push(c);
+        else
+        {
+            if(stk.empty()) return false;
+            else stk.pop();
+        }
+    }
+    if(!stk.empty()) return false;
+
+    return answer;
+}
