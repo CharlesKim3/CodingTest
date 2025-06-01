5f9e664b6e9c2b2c27b1e8af215c6e5d95e8ca4a
CharlesKim3 <tpwls971105@naver.com>
2025-06-02 00:28:31 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -6,34 +6,22 @@ int main()
 	int N;
 	cin >> N;
 	
-	int space = N;
-	int star = 1;
 	for (int i = 1; i <= N - 1; ++i)
 	{
-		for (int j = 0; j < space - i; ++j)
-			cout << ' ';
-		for (int j = 1; j <= star; ++j)
-			cout << '*';
-
-		star += 2;
+		for (int j = N - i; j >= 1; --j) cout << " ";
+		for (int j = 0; j < i * 2 - 1; ++j) cout << "*";
 		cout << endl;
 	}
-
-	for (int i = 0; i < N * 2 - 1; ++i)
-		cout << '*';
+	for (int i = 0; i < N * 2 - 1; ++i) cout << "*";
 	cout << endl;
-	star -= 2;
 
 	for (int i = N - 1; i >= 1; --i)
 	{
-		for (int j = 0; j < space - i; ++j)
-			cout << ' ';
-		for (int j = 1; j <= star; ++j)
-			cout << '*';
-
-		star -= 2;
+		for (int j = N - i; j >= 1; --j) cout << " ";
+		for (int j = 0; j < i * 2 - 1; ++j) cout << "*";
 		cout << endl;
 	}
+	
 
 
 	return 0;
