0ab01e519a7398c98bd1b2706f0de9696c9fa22e
CharlesKim3 <tpwls971105@naver.com>
2025-07-30 20:46:16 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.76 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer(2,0);
+    int count = 0;
+    
+    while(s.size() > 1)
+    {    
+        sort(s.begin(),s.end());
+        string::iterator it = s.begin();
+        while(*it == '0') ++it;
+        answer[1] += it - s.begin();
+        
+        s.erase(remove(s.begin(),s.end(),'0'),s.end());
+        int length = s.size();
+        s = "";
+        while(length >= 1)
+        {
+            s += (length % 2) + '0';
+            length /= 2;
+        }
+        reverse(s.begin(),s.end());
+        
+        ++answer[0];
+    }
+    
+    return answer;
+}
