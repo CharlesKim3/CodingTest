5935631e4584558e6632cad20581c3946dce6874
CharlesKim3 <tpwls971105@naver.com>
2025-08-05 23:46:05 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.85 ms, Memory: 5.19 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949#) 
 
 ### 성능 요약
 
-메모리: 5.62 MB, 시간: 2.92 ms
+메모리: 5.19 MB, 시간: 2.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 31일 21:18:19
+2025년 08월 05일 23:44:04
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -3,23 +3,27 @@
 
 using namespace std;
 
+vector<int> result (vector<int> a, vector<vector<int>> b)
+{
+    vector<int> tmp;
+    for(int i=0; i<b[0].size(); ++i)
+    {
+        int sum = 0;
+        for(int j=0; j<b.size(); ++j)
+        {
+            sum += a[j] * b[j][i];
+        }
+        tmp.push_back(sum);
+    }
+    return tmp;
+}
+
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
     vector<vector<int>> answer;
-
-    for(int i = 0; i < arr1.size(); ++i)
+    
+    for(int i=0; i<arr1.size(); ++i)
     {
-        vector<int> tmp;
-        for(int j = 0; j < arr2[0].size(); ++j)
-        {
-            int sum = 0;
-            for(int k = 0; k < arr2.size(); ++k)
-            {
-                sum += arr1[i][k] * arr2[k][j];
-            }
-            tmp.push_back(sum);
-        }
-        answer.push_back(tmp);
+        answer.push_back(result(arr1[i], arr2));
     }
-
     return answer;
 }
