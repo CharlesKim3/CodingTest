14e6d123915b3a78e0c1a30a2148ffc7aefc1a3e
CharlesKim3 <tpwls971105@naver.com>
2025-09-07 15:48:57 +0900

    [level 0] Title: 글자 지우기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string my_string, vector<int> indices) {
+    string answer = "";
+    
+    for(int i=0; i<my_string.size(); ++i)
+    {
+        auto it = find(indices.begin(), indices.end(), i);
+        if(it == indices.end())
+            answer += my_string[i];
+    }
+    
+    return answer;
+}
