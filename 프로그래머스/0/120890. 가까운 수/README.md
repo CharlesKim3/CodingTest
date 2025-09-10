cf22695aa29c06b6b6e49383ae2f3f016c68d226
CharlesKim3 <tpwls971105@naver.com>
2025-09-10 21:40:45 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/README.md
+++ 프로그래머스/0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890) 
 
 ### 성능 요약
 
-메모리: 3.68 MB, 시간: 0.01 ms
+메모리: 3.73 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 10일 21:18:15
+2025년 09월 10일 21:37:30
 
 ### 문제 설명
 
--- 프로그래머스/0/120890. 가까운 수/가까운 수.cpp
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.cpp
@@ -4,19 +4,18 @@
 using namespace std;
 
 int solution(vector<int> array, int n) {
-    array.push_back(n);
     sort(array.begin(),array.end());
-    auto it = find(array.begin(),array.end(), n);
-
-    int idx = it - array.begin();
-    if(idx == array.size() - 1)
-        return array[idx - 1];
-    else if(idx == 0)
-        return array[1];
-    else
+    int answer = array[0];
+    int dis = abs(n - array[0]);
+    
+    for(int i=1; i<array.size(); ++i)
     {
-        int prev = array[idx - 1];
-        int next = array[idx + 1];
-        return abs(n - prev) <= abs(n - next) ? prev : next;
+        if(dis > abs(n - array[i]))
+        {
+            answer = array[i];
+            dis = abs(n - array[i]);
+        }
     }
+    
+    return answer;
 }
