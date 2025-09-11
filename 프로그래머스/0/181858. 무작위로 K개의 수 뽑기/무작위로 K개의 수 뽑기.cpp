428ac1ac13fc809ac540cf68db22982dfa47ffc2
CharlesKim3 <tpwls971105@naver.com>
2025-09-11 17:14:46 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 57.93 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr, int k) {
+    vector<int> answer;
+    
+    for(int i=0; i<arr.size(); ++i)
+    { 
+        auto it = find(answer.begin(),answer.end(), arr[i]);
+        if(it == answer.end())
+            answer.push_back(arr[i]);
+    }
+    
+    if(answer.size() >= k)
+        answer.erase(answer.begin() + k, answer.end());
+    else
+    {
+        int repeat = k - answer.size();
+        for(int i=0; i<repeat; ++i)
+            answer.push_back(-1);
+    }
+    
+    return answer;
+}
