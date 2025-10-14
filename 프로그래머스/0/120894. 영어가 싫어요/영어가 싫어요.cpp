c56d0a4d50fa6b8f02abf702bbde319ef40855b4
CharlesKim3 <tpwls971105@naver.com>
2025-10-14 22:23:18 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
@@ -0,0 +1,73 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(string numbers) {
+    long long answer = 0;
+    string result = "";
+    string tmp = "";
+    
+    for(int i = 0; i<numbers.size(); ++i)
+    {
+        tmp += numbers[i];
+        if(tmp == "one")
+        {
+            result += "1";
+            tmp = "";
+        }
+        else if(tmp == "two")
+        {
+            result += "2";
+            tmp = "";
+        }
+        else if(tmp == "three")
+        {
+            result += "3";
+            tmp = "";
+        }
+        else if(tmp == "four")
+        {
+            result += "4";
+            tmp = "";
+        }
+        else if(tmp == "five")
+        {
+            result += "5";
+            tmp = "";
+        }
+        else if(tmp == "six")
+        {
+            result += "6";
+            tmp = "";
+        }
+        else if(tmp == "seven")
+        {
+            result += "7";
+            tmp = "";
+        }
+        else if(tmp == "eight")
+        {
+            result += "8";
+            tmp = "";
+        }
+        else if(tmp == "nine")
+        {
+            result += "9";
+            tmp = "";
+        }
+        else if(tmp == "zero")
+        {
+            result += "0";
+            tmp = "";
+        }
+    }
+    
+    for(int i=0; i<result.size(); ++i)
+    {
+        answer *= 10;
+        answer += result[i] - '0';
+    }
+    
+    return answer;
+}
