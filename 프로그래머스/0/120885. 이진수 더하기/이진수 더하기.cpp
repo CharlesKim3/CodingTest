3d060c4f0284b1828d11305633e53c9a1ba6b80b
CharlesKim3 <tpwls971105@naver.com>
2025-10-15 22:52:11 +0900

    [level 0] Title: 이진수 더하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
@@ -6,14 +6,20 @@ using namespace std;
 string solution(string bin1, string bin2) {
     string answer = "";
     
-    int num = stoi(bin1, nullptr, 2) + stoi(bin2, nullptr, 2);
+    int num1 = 0;
+    for(auto b : bin1)
+        num1 = num1 * 2 + (b - '0');
+    int num2 = 0;
+    for(auto b : bin2)
+        num2 = num2 * 2 + (b - '0');
+    int sum = num1 + num2;
     
-    if(num > 0)
+    if(sum > 0)
     {
-            while(num)
+        while(sum)
         {
-            answer += to_string(num % 2);
-            num /= 2;
+            answer += to_string(sum % 2);
+            sum /= 2;
         }
         reverse(answer.begin(),answer.end());
     }
