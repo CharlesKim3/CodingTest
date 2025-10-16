d61397746b61627670bc7021d92faa3eba9d3032
CharlesKim3 <tpwls971105@naver.com>
2025-10-16 21:37:31 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -5,18 +5,8 @@ using namespace std;
 
 int solution(vector<int> numbers) {
     int answer = 0;
-    int idx = numbers.size() -1;
-    sort(numbers.begin(), numbers.end(),greater<int>());
+    sort(numbers.begin(), numbers.end());
+    int idx = numbers.size();
     
-    if(numbers.front() > 0 && numbers.back() > 0)
-        answer = numbers[0] * numbers[1];
-    else if(numbers.front() > 0 && numbers.back() < 0)
-    {
-        answer = numbers[0] * numbers[1] > numbers[idx] * numbers[idx -1] ?
-            numbers[0] * numbers[1] : numbers[idx] * numbers[idx -1];
-    }
-    else if(numbers.front() < 0 && numbers.back() < 0)
-        answer = numbers[idx] * numbers[idx -1];
- 
-    return answer;
+    return max(numbers[0] * numbers[1], numbers[idx -1] * numbers[idx - 2]);
 }
