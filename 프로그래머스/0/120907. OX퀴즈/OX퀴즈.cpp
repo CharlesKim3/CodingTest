1c8b887fcc0a866a70eb0128d2d7e4fab986afef
CharlesKim3 <tpwls971105@naver.com>
2025-10-22 15:52:08 +0900

    [level 0] Title: OX퀴즈, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.cpp
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.cpp
@@ -6,32 +6,30 @@ using namespace std;
 vector<string> solution(vector<string> quiz) {
     vector<string> answer;
     
-    for(int i=0; i<quiz.size(); ++i)
+    for(const auto& q : quiz)
     {
-        vector<int> num;
-        stringstream ss(quiz[i]);
-        string s;
-        int cnt = 0;
+        string x, y, z, op;
+        stringstream ss(q);
+        ss >> x; ss >> op; ss >>y; ss >> z; ss >> z;
         
-        while(ss >> s)
+        int result;
+        switch(op[0])
         {
-            if(cnt == 0 || cnt == 2 || cnt == 4)
-            {
-                if(s[0] != '-') num.push_back(stoi(s));
-                else num.push_back(stoi(s.substr(1,s.size() - 1)) * -1);
-            }
-            else if(cnt == 1)
-            {
-                if(s == "+") num.push_back(1);
-                else if(s == "-") num.push_back(-1);
-            }
-            ++cnt;
+            case '+':
+                result = stoi(x) + stoi(y);
+                break;
+                
+            case '-':
+                result = stoi(x) - stoi(y);
+                break;
+                
+            default:
+                break;
         }
         
-        if(num[0] + num[1] * num[2] == num[3]) answer.push_back("O");
+        if(result == stoi(z)) answer.push_back("O");
         else answer.push_back("X");
     }
-    
     
     return answer;
 }
