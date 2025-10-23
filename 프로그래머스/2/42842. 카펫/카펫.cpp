272cf919b567b54291fd6b6632a4001e4a7cf009
CharlesKim3 <tpwls971105@naver.com>
2025-10-23 19:57:44 +0900

    [level 2] Title: 카펫, Time: 1.13 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -7,23 +7,18 @@ vector<int> solution(int brown, int yellow) {
     vector<int> answer(2,0);
     int size = brown + yellow;
     int x = 3; int y = 3;
-    
     while(size != x * y)
     {
-        y = 3;
-        if(size == x * y)
-            break;
-        
         while(x > y)
         {
             ++y;
-            if(size == x * y && yellow == (x - 2) * (y - 2))
+            if(x * y == size && yellow == (x-2) * (y-2))
             {
                 answer[0] = x; answer[1] = y;
                 return answer;
             }
         }
-        ++x;
+        ++x; y = 3; 
     }
     
     answer[0] = x; answer[1] = y;
