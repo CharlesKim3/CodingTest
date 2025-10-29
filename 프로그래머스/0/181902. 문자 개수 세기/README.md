7b5262ae273135f8a21a1b4ba9b006d1e4631b53
CharlesKim3 <tpwls971105@naver.com>
2025-10-30 01:50:10 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/README.md
+++ 프로그래머스/0/181902. 문자 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자 개수 세기 - 181902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181902?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181902) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.02 ms
+메모리: 4.21 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 30일 01:46:05
+2025년 10월 30일 01:48:49
 
 ### 문제 설명
 
--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
@@ -5,13 +5,11 @@ using namespace std;
 
 vector<int> solution(string my_string) {
     vector<int> answer(52,0);
-    vector<int> test;
-    // 65 ~ 90 
-    // 97 ~ 122
+
     for(const auto& s : my_string)
     {
-        if('A'<= s && s <= 'Z') answer[s - 'A']++;
-        else if('a' <= s && s <= 'z') answer[s - 'a' + 26]++;
+        if(s <= 'Z') answer[s - 'A']++;
+        else if(s >= 'a') answer[s - 'a' + 26]++;
     }
     return answer;
 }
