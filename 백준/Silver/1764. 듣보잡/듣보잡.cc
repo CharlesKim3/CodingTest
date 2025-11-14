60fedce50c02478b3fceb103f5aa4f5a2eb6e4f7
CharlesKim3 <tpwls971105@naver.com>
2025-11-14 13:13:27 +0900

    [Silver IV] Title: 듣보잡, Time: 244 ms, Memory: 12700 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,28 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 int main()
 {
 	int N, M;
 	cin >> N >> M;
-
 	unordered_map<string, int> m;
 	for (int i = 0; i < N + M; ++i)
 	{
-		string str;
+		string str = "";
 		cin >> str;
-		if (m.find(str) == m.end())
-			m.insert({ str,1 });
-		else
-			m[str]++;
+		m[str]++;
 	}
 
 	int result = 0;
 	vector<string> v;
-	for (pair<string, int> m : m)
+	for (const auto& m : m)
 	{
 		if (m.second > 1)
 		{
@@ -35,6 +31,7 @@ int main()
 	cout << result << endl;
 	for (const auto& v : v)
 		cout << v << endl;
+	
 
 	return 0;
 }
