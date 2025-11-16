b50db6db7812c3af71f4667b3f68e8dc91b1383b
CharlesKim3 <tpwls971105@naver.com>
2025-11-16 19:21:55 +0900

    [level 2] Title: 구명보트, Time: 1.78 ms, Memory: 4.72 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    sort(people.begin(),people.end());
+    int front = 0;
+    int back = people.size() - 1;
+    
+    while(front <= back)
+    {
+        if(people[front] + people[back] <= limit)
+        {
+            ++front;
+            --back;
+        }
+        else
+        {
+            --back;
+        }
+        ++answer;
+    }
+    
+    return answer;
+}
