45ea25ca9043c3d171e04b65f779afb8248561ec
CharlesKim3 <tpwls971105@naver.com>
2025-11-20 21:46:51 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int N, M, V;
+vector<vector<int>> graph;
+vector<bool> visited;
+
+void dfs(int start)
+{
+	visited[start] = true;
+	cout << start << " ";
+	for (int i = 0; i < graph[start].size(); ++i)
+	{
+		int y = graph[start][i];
+		if (!visited[y])
+			dfs(y);
+	}
+}
+
+void bfs(int start)
+{
+	queue<int> q;
+	q.push(start);
+	visited[start] = true;
+	while (!q.empty())
+	{
+		int x = q.front();
+		q.pop();
+		cout << x << " ";
+		for (int i = 0; i < graph[x].size(); ++i)
+		{
+			int y = graph[x][i];
+			if (!visited[y])
+			{
+				q.push(y);
+				visited[y] = true;
+			}
+		}
+	}
+}
+
+int main()
+{
+	
+	cin >> N >> M >> V;
+	graph.resize(N + 1, vector<int>());
+	visited.resize(N + 1, false);
+
+	for (int i = 0; i < M; ++i)
+	{
+		int start, end;
+		cin >> start >> end;
+		graph[start].push_back(end);
+		graph[end].push_back(start);
+	}
+
+	for (auto& g : graph)
+		sort(g.begin(), g.end());
+
+	dfs(V);
+	for (auto v : visited)
+		v = false;
+
+	cout << endl;
+	bfs(V);
+	return 0;
+}
