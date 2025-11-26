3e53ec6c47b606b08ee2c1e5bce001cc6dd89ec9
CharlesKim3 <tpwls971105@naver.com>
2025-11-26 20:27:41 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int solution(vector<int> arr) {
     int answer = 0;
     
-    answer = arr[0];
+    answer = arr.front();
     for(int i=1; i<arr.size(); ++i)
     {
         answer = lcm(answer, arr[i]);
