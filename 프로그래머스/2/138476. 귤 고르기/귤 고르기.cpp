e1aa3494697e7de4f33fee35741b28a3f472d7e3
CharlesKim3 <tpwls971105@naver.com>
2025-12-01 15:15:39 +0900

    [level 2] Title: 귤 고르기, Time: 66.62 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int solution(int k, vector<int> tangerine) {
+    int answer = 0;
+    map<int, int> m;
+    for(auto t : tangerine)
+    {
+        if(m.find(t) == m.end())
+            m.insert({t,1});
+        else
+            m[t]++;
+    }
+    
+    vector<pair<int, int>> v(m.begin(), m.end());
+    sort(v.begin(), v.end(), [](auto l, auto r){
+        return l.second > r.second;
+    });
+    
+    for(auto [key, value] : v)
+    {
+        if(k <= 0) break;
+        k -= value;
+        ++answer;
+    }
+    
+    return answer;
+}
