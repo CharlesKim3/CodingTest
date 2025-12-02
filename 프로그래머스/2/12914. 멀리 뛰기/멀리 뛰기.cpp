5d347b387cf7ec969fc4aef1e6e25891dc828512
CharlesKim3 <tpwls971105@naver.com>
2025-12-02 20:37:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    int prev1 = 1, prev2 = 2;
+    
+    for(int i=3; i<=n; ++i)
+    {
+        answer = (prev2 + prev1) % 1234567;
+        prev1 = prev2;
+        prev2 = answer;
+    }
+    
+    if(answer == 0) answer = n;
+    
+    return answer;
+}
